934b93dd3fce1d58778552f983c2f782d4b26080
Mike Calvanese <mfcalvanese@gmail.com>
2017-10-23 14:29:59 -0400

    make the deploy script easier to read and configure
    also, add a transfer of ownership so owner address can be set after deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,18 +3,44 @@ const moment = require('moment')
 const AuthenticIDCrowdsale = artifacts.require('AuthenticIDCrowdsale')
 
 module.exports = function (deployer) {
-  const threeMinutesFromNow = moment().add(0, 'minute')
-  const oneMonthFromNow = moment().add(1, 'month')
-  const startTime = threeMinutesFromNow.unix()
-  const endTime = oneMonthFromNow.unix()
+  const athauCentRate = 15
+  const etherUSDRate = 300
+
+  // startTime: timestamp for public sale start
+  const startTime = moment().add(3, 'minute').unix()
+
+  // endTime: timestamp for public sale end
+  const endTime = moment().add(1, 'month').unix()
+
+  // rate: number of ATHAU tokens sold for 1 ether
+  const rate = Math.round(etherUSDRate * 100 / athauCentRate)
+
+  // walletAddress: address of the wallet where ether sent with public sale
+  //                buys will be forwarded
+  const walletAddress = '0xa5f471b71baaf9cd9044c1ea8cfc4dab97e3040a'
+
+  // authenticIDAddress: address where 35% (350 million) ATHAU token grant
+  //                     for Authentic ID will be allocated
+  const authenticIDAddress = '0x70b34d655564918d2bfb21c136e30faf2c6bb2c5'
+
+  // adoptionRewardsAddress: address where 35% (350 million) ATHAU token
+  //                         grant for adoption rewards will be allocated
+  const adoptionRewardsAddress = '0xf705fb71b53cf701ac2034d6dbf00aee2a101d55'
+
+  // ownerAddress: address that will own the deployed contract. This address
+  //               will be the only address with the ability to allocate
+  //               tokens and create bounty grants
+  const ownerAddress = '0xe2b3204f29ab45d5fd074ff02ade098fbc381d42'
 
   deployer.deploy(
     AuthenticIDCrowdsale,
     startTime,
     endTime,
-    1200,
-    '0x00000000000000000000000000000000000000a1',
-    '0x70b34d655564918d2bfb21c136e30faf2c6bb2c5',
-    '0xf705fb71b53cf701ac2034d6dbf00aee2a101d55'
-  )
+    rate,
+    walletAddress,
+    authenticIDAddress,
+    adoptionRewardsAddress
+  ).then(() => {
+    AuthenticIDCrowdsale.at(AuthenticIDCrowdsale.address).transferOwnership(ownerAddress)
+  })
 }
